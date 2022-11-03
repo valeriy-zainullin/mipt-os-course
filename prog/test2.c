4d776f7641ecfd02f0124b641aab75ee5f5f224d
Valeriy Zainullin <valeriy.zainullin@yandex.ru>
2022-11-03 20:53:13 +0300

    Lab 3: patch test files to show the work.

--- prog/test2.c
+++ prog/test2.c
@@ -11,10 +11,16 @@ void (*volatile GRADE3_FUNC)(unsigned);
 void (*volatile xc(GRADE3_FAIL, GRADE3_PFX1))(void);
 #endif
 
+void great_function_impl() { cprintf("Great function pointer was not overriden!\n"); }
+void (*volatile great_function)(void) = great_function_impl;
+
+
 void
 umain(int argc, char **argv) {
     int test2_i;
     int test2_j;
+
+    great_function();
 
 #if !defined(GRADE3_TEST)
     cprintf("TEST2 LOADED.\n");
@@ -25,9 +31,12 @@ umain(int argc, char **argv) {
     }
 #endif
 
+    cprintf("TEST2 STARTED.\n");
     for (test2_j = 0; test2_j < 5; ++test2_j) {
-        for (test2_i = 0; test2_i < 10000; ++test2_i)
+        for (test2_i = 0; test2_i < 100; ++test2_i)
+            cprintf("TEST2 LOADED.\n");
             ;
         sys_yield();
     }
+    cprintf("TEST2 DONE.\n");
 }
