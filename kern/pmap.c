525569c74b5433b620cc2f1099e57427c7bf9311
Vitaly Cheptsov <cheptsov@ispras.ru>
2022-12-29 14:10:23 +0300

    lab6: fix compilation with llvm 15

--- kern/pmap.c
+++ kern/pmap.c
@@ -621,6 +621,7 @@ detect_memory(void) {
             /* Attach memory described by memory map entry described by start
              * of type type*/
             // LAB 6: Your code here
+            (void)type;
 
 
 
